26b86780117deeb7ee4f419edba872f7b2094524
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-07-25 22:44:02 +0530

    img not taking full width resolved

---
+++ api.js
@@ -0,0 +1,10 @@
+// const fetchAPI = async (IP) => {
+//   const res = await fetch(
+//     "https://geo.ipify.org/api/v2/country?apiKey=at_orKP6AZFrj6WyEgM2HFvaYBtnlNXB&ipAddress=103.226.202.38"
+//   );
+
+//   const location = await res.json();
+//   console.log(location);
+// };
+
+// fetchAPI(12);
